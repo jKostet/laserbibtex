88507550088d298a8581d4c5b4ba2073209f21cf
Tintti Rahikainen <tintti.rahikainen@gmail.com>
2016-04-12 17:32:13 +0300

    encode special chars

--- app/models/concerns/model_to_bib.rb
+++ app/models/concerns/model_to_bib.rb
@@ -5,4 +5,19 @@ module ModelToBib
     
   end
 
+  def encode_special_chars(string)
+    tabel = [
+        ["ä", "\"{a}"],
+        ["Ä", "\"{A}"],
+        ["ö", "\"{o}"],
+        ["Ö", "\"{O}"],
+    ]
+
+    tabel.each do |t|
+      string = string.gsub(t[0], t[1])
+    end
+
+    string
+  end
+
 end
