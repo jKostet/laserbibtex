b38f627eea72228025276c4ca2cade9d13b5e236
Mikko Markkinen <mikko.markkinen@gmail.com>
2016-04-12 17:04:40 +0300

    modified modeltobib

--- app/models/concerns/model_to_bib.rb
+++ app/models/concerns/model_to_bib.rb
@@ -2,7 +2,7 @@ module ModelToBib
   extend ActiveSupport::Concern
 
   def toBibTex
-    
+    str = '@' + self.model_name.singular + '{' + self.reference + ',' + '\n' +
+    self.attribute_names.to_s
   end
-
 end
