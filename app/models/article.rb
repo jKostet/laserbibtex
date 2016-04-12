14de3b661dc43e0b6912e38b355ca6632760520a
Mikko Markkinen <mikko.markkinen@gmail.com>
2016-04-12 11:26:12 +0300

    added validates to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ActiveRecord::Base
+  validates :author, presence: true
+  validates :title, presence: true
+  validates :journal, presence: true
+  validates :year, presence: true
+  validates :volume, presence: true
 end
