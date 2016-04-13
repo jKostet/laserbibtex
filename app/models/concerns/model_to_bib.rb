b72ac11aef588b2184d0cc633a5a559631e8298b
jKostet <juho.kostet@helsinki.fi>
2016-04-13 13:12:43 +0300

    Improve BibTeX file, cut nil values and empty strings

--- app/models/concerns/model_to_bib.rb
+++ app/models/concerns/model_to_bib.rb
@@ -4,17 +4,14 @@ module ModelToBib
   def toBibTex
     str = '@' + self.model_name.singular + '{' + self.reference + ',' + "\n"
 
-    self.attributes.reject{|x| x == id or x == reference}.each { |key, value|
+    self.attributes.except("id", "reference", "created_at", "updated_at").each { |key, value|
+        next unless value.present?
 
-      if value.to_s != "" && key != "id" && key != "reference" && key != "created_at" && key != "updated_at"
         line = "\t" << key << ' = {' << value.to_s << '},' << "\n"
         str << encode_special_chars(line)
-       #puts "#{key} is #{value}"
-
-      end
     }
-
-    return str
+    str << "}\n\n"
+    str
   end
 
 
