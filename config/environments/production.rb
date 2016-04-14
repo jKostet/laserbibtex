1b59acdd19e8c7ce627583a288c7193ad30fb8c2
jKostet <juho.kostet@helsinki.fi>
2016-04-14 13:24:04 +0300

    Attempt fix kukkahattus

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
+
+  # Do serve and compile assets for Heroku
+  config.serve_static_assets = true
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
