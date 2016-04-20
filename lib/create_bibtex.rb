ba2f2d2e38a0a170564f55174fd1a5d0579b5d46
Mikko Markkinen <mikko.markkinen@gmail.com>
2016-04-20 11:28:17 +0300

    all refs and create bibtex fix to include all models

--- lib/create_bibtex.rb
+++ lib/create_bibtex.rb
@@ -2,12 +2,19 @@ class CreateBibtex
 
   def self.createBibString
 
-    allRefs = Article.all
+    allRefs = []
+
+    Dir[Rails.root.join('app/models/*.rb').to_s].each do |filename|
+      model_name = File.basename(filename, '.rb').camelize.constantize
+      next unless model_name.ancestors.include?(ActiveRecord::Base)
+        allRefs = allRefs + model_name.all
+    end
+
     str = ""
     allRefs.each {|object| str << object.toBibTex}
 
     str << "\n"
-    
+
     return str
 
   end
