6e687131e49b3fe8e4476d06dad5bde188b828fe
Mikko Markkinen <mikko.markkinen@gmail.com>
2016-04-27 14:29:35 +0300

    fixed ReferenceValidator

--- app/models/reference_validator.rb
+++ app/models/reference_validator.rb
@@ -1,11 +1,18 @@
 class ReferenceValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
+
     Dir[Rails.root.join('app/models/*.rb').to_s].each do |filename|
       model_name = File.basename(filename, '.rb').camelize.constantize
       next unless model_name.ancestors.include?(ActiveRecord::Base)
         unless (model_name.find_by reference: value).nil?
-          record.errors[attribute] << (options[:message] || " must be unique")
+          object = model_name.find_by reference: value
+
+          if (object.model_name.name == record.model_name.name && object.id == record.id)
+              return true
+          else
+            record.errors[attribute] << (options[:message] || " must be unique")
+          end
       end
     end
   end
