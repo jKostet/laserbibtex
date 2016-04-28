02a0c017c3e655ad18ee4a98c84e2450692751e3
Maximilian Remming <maxremming@gmail.com>
2016-04-28 17:29:07 +0300

    Added better search?????

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,15 +2,10 @@ class SearchController < ApplicationController
 
   def search_results
     query = params[:q]
-    allRefs = GetCitations.all_citations
     @results = []
-    allRefs.each do |ref|
-      ref.attributes.except("id", "created_at", "updated_at").each do |name, value|
-        if (value.to_s.downcase.include?(query.downcase))
-          @results << ref
-        end
-      end
-    end
+    out << Article.advanced_search(query)
+    out << Book.advanced_search(query)
+    out << Inproceeding.advanced_search(query)
     @results = @results.uniq
   end
 
