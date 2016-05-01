90b5a600a4b20ad32932d9bac883405090c7b39b
Nea <nea.pirttinen@cs.helsinki.fi>
2016-05-01 23:56:14 +0300

    Refactored most of the interface tests

--- spec/features/all_references_page_spec.rb
+++ spec/features/all_references_page_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe "All references page" do
+  let(:article) { FactoryGirl.build(:article) }
+  let(:book) { FactoryGirl.create(:book) }
+  let(:inproceeding) { FactoryGirl.build(:inproceeding) }
+
   it "should not have anything before something has been created" do
     visit references_path
 
@@ -10,28 +14,25 @@ describe "All references page" do
     expect(page).to have_content 'author'
     expect(page).to have_content 'title'
 
-    expect(page).not_to have_content("asd")
+    expect(page).not_to have_content("2016")
   end
 
   it "lists added objects correctly" do
-    article = Article.new(reference: "ALL00", author: "asd", title: "test", journal: "asd journal", year: 2000, volume: 1)
     expect(article).to be_valid
     article.save
 
-    book = Book.new(reference: "ALL01", author: "asdasd", title: "testtest", publisher: "asd publish", year: 2000)
     expect(book).to be_valid
     book.save
 
-    inproceeding = Inproceeding.new(reference: "ALL02", author: "asdasdasd", title: "testtesttest", booktitle: "asd booktitle", year: 3000)
     expect(inproceeding).to be_valid
     inproceeding.save
 
     visit references_path
 
-    expect(page).to have_content 'ALL00'
-    expect(page).to have_content 'ALL01'
-    expect(page).to have_content 'asdasd'
-    expect(page).to have_content 'asdasdasd'
-    expect(page).to have_content 'testtesttest'
+    expect(page).to have_content 'A2016'
+    expect(page).to have_content 'B2016'
+    expect(page).to have_content 'ville vallaton'
+    expect(page).to have_content 'puffet on hyvää'
+    expect(page).to have_content 'purjojen sielunelämä'
   end
 end
--- spec/features/show_bibtex_page_spec.rb
+++ spec/features/show_bibtex_page_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe "Show BibTeX page" do
+  let(:article) { FactoryGirl.build(:article) }
+  let(:book) { FactoryGirl.create(:book) }
+  let(:inproceeding) { FactoryGirl.build(:inproceeding) }
+
   it "should not have anything before something has been created" do
     visit bibtex_path
 
@@ -9,30 +13,26 @@ describe "Show BibTeX page" do
     expect(page).to have_content 'Articles'
     expect(page).to have_content 'Books'
 
-    expect(page).not_to have_content("asd")
+    expect(page).not_to have_content("2016")
   end
 
   it "lists added objects correctly" do
-    article = Article.new(reference: "ARTBIB", author: "asd", title: "test", journal: "asd journal", year: 2000, volume: 1)
     expect(article).to be_valid
     article.save
 
-    book = Book.new(reference: "BOOBIB", author: "asdasd", title: "testtest", publisher: "asd publish", year: 2000)
     expect(book).to be_valid
     book.save
 
-    inproceeding = Inproceeding.new(reference: "INPBIB", author: "asdasdasd", title: "testtesttest", booktitle: "asd booktitle", year: 3000)
     expect(inproceeding).to be_valid
     inproceeding.save
 
     visit bibtex_path
 
-    expect(page).to have_content '@article{ARTBIB,'
-    expect(page).to have_content '@book{BOOBIB,'
-    expect(page).to have_content '@inproceedings{INPBIB,'
-    expect(page).to have_content 'author = {asd},'
-    expect(page).to have_content 'title = {testtest},'
-    expect(page).to have_content 'year = {3000},'
-    expect(page).to have_content 'publisher = {asd publish},'
+    expect(page).to have_content '@article{A2016,'
+    expect(page).to have_content '@book{B2016,'
+    expect(page).to have_content '@inproceedings{I2015,'
+    expect(page).to have_content 'author = {pirjospiruliina},'
+    expect(page).to have_content 'year = {2015},'
+    expect(page).to have_content 'publisher = {asd publishing},'
   end
 end
