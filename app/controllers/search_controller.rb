0e56ff8dfc45172291719966433e5f300dad3f92
Tintti Rahikainen <tintti.rahikainen@gmail.com>
2016-05-03 12:57:11 +0300

    multiple AND for search added

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,16 +2,41 @@ class SearchController < ApplicationController
 
   def search_results
     query = params[:q]
-    allRefs = GetCitations.all_citations
+    @queries = query.split(" AND ")
     @results = []
-    allRefs.each do |ref|
-      ref.attributes.except("id", "created_at", "updated_at").each do |name, value|
-        if (value.to_s.downcase.include?(query.downcase))
-          @results << ref
-        end
+
+    all_citations = GetCitations.all_citations
+
+    all_citations.each do |citation|
+      if check_queries(citation)
+        @results << citation
       end
     end
+
     @results = @results.uniq
+  end
+
+  # Checks if given citation includes all the given queries
+  def check_queries(citation)
+    found = false
+    @queries.each do |q|
+      found = check_citation(citation,q)
+      # if the citation doesn't include a query, no need to check the others
+      if !found
+        break
+      end
+    end
+    found
+  end
+
+  # Checks if given citation includes a single query
+  def check_citation(citation, query)
+    citation.attributes.except("id", "created_at", "updated_at").each do |name, value|
+      if (value.to_s.downcase.include?(query.downcase))
+        return true
+      end
+    end
+    false
   end
 
 end
