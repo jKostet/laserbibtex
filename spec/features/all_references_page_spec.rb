408fd9aa645bfe2d49e9b03afba054982dd1a47b
Mikko Markkinen <mikko.markkinen@gmail.com>
2016-05-03 15:10:01 +0300

    added some tests for tags

--- spec/features/all_references_page_spec.rb
+++ spec/features/all_references_page_spec.rb
@@ -35,4 +35,36 @@ describe "All references page" do
     expect(page).to have_content 'puffet on hyvää'
     expect(page).to have_content 'purjojen sielunelämä'
   end
+
+end
+
+describe "All reference page shows correct tag information" do
+  let(:article_with_tags) { FactoryGirl.build(:article_with_tags) }
+  let(:book_with_tags) { FactoryGirl.build(:book_with_tags) }
+  let(:inproceeding_with_tags) { FactoryGirl.build(:inproceeding_with_tags) }
+
+  it "lists added objects correctly" do
+    expect(article_with_tags).to be_valid
+    article_with_tags.save
+
+    expect(book_with_tags).to be_valid
+    book_with_tags.save
+
+    expect(inproceeding_with_tags).to be_valid
+    inproceeding_with_tags.save
+
+    visit references_path
+
+    save_and_open_page
+
+    expect(page).to have_content 'A2016'
+    expect(page).to have_content 'B2016'
+    expect(page).to have_content 'ville vallaton'
+    expect(page).to have_content 'puffet on hyvää'
+    expect(page).to have_content 'purjojen sielunelämä'
+
+    expect(page).to have_content 'jaatelo, runsas'
+    expect(page).to have_content 'spirulina, ruokainen'
+    expect(page).to have_content 'raikas, fresh'
+  end
 end
